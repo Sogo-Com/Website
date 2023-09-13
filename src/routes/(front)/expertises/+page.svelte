25ee474f6d91a0e9315ea42e881d4b2536a69df0
unknown <guillianvibert90@gmail.com>
2023-09-14 00:13:48 +0200

    feat: expertises

--- src/routes/(front)/expertises/+page.svelte
+++ src/routes/(front)/expertises/+page.svelte
@@ -1,7 +1,21 @@
 <script>
 	import { onMount } from 'svelte';
-
-	onMount((_) => {});
+	import Swiper from 'swiper';
+	import { Pagination } from 'swiper/modules';
+	let links;
+	let wrapper;
+	onMount((_) => {
+		const swiper= new Swiper('.swiper-container', {
+			modules: [Pagination],
+			// loop: true,
+			
+			// If we need pagination
+			pagination: {
+				clickable: true,
+				el: '.pagination'
+			}
+		});
+	});
 </script>
 
 <div id="top">
@@ -33,6 +47,144 @@
 			</div>
 		</div>
 	</div>
+</div>
+<div class="pink">
+	<div bind:this={links} class="links">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			class="link"
+			on:click={() => {
+				changeProjet(0);
+			}}
+		>
+			Rédaction
+		</div>
+
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			class="link"
+			on:click={() => {
+				changeProjet(1);
+			}}
+		>
+			Relations presses
+		</div>
+
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			class="link"
+			on:click={() => {
+				changeProjet(2);
+			}}
+		>
+			Graphisme
+		</div>
+
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			class="link"
+			on:click={() => {
+				changeProjet(3);
+			}}
+		>
+			Réseaux sociaux
+		</div>
+
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			class="link"
+			on:click={() => {
+				changeProjet(4);
+			}}
+		>
+			Photos & Vidéos
+		</div>
+
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			class="link"
+			on:click={() => {
+				changeProjet(5);
+			}}
+		>
+			Evenements
+		</div>
+	</div>
+
+	<div class="contenu">
+		<div class="image">
+			<img src="/images/sogo-pink.png" alt="sogo" />
+		</div>
+		<div class="texte">
+			<h3>Lorem Ipsum</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque facilisis orci at
+				luctus. Donec in euismod nibh. Ut mollis enim neque, et tincidunt nulla pulvinar
+				condimentum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque
+				facilisis orci at luctus. Donec in euismod nibh. Ut mollis enim neque, et tincidunt nulla
+				pulvinar condimentum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+				scelerisque facilisis orci at luctus. Donec in euismod nibh. Ut mollis enim neque, et
+				tincidunt nulla pulvinar condimentum.
+			</p>
+		</div>
+	</div>
+</div>
+<div class="blanc">
+	<div class="wrapper">
+		<div class="bloc desc-gauche">
+			<h2>
+				<span class="grey">Nos expertises</span>
+			</h2>
+		</div>
+	</div>
+	<div class="infoSlide">
+		<div class="infos">
+			<div class="medias">
+				<img src="/images/real-1.svg" alt="Real" />
+				<img src="/images/real-2.svg" alt="Real" />
+				<img src="/images/real-3.svg" alt="Real" />
+				<img src="/images/real-4.svg" alt="Real" />
+				<img src="/images/real-5.svg" alt="Real" />
+				<img src="/images/real-6.svg" alt="Real" />
+			</div>
+			<div class="texte">
+				<h3>Lorem Ipsum</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque facilisis orci at
+					luctus. Donec in euismod nibh. Ut mollis enim neque, et tincidunt nulla pulvinar
+					condimentum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque
+					facilisis orci at luctus. Donec in euismod nibh. Ut mollis enim neque, et tincidunt nulla
+					pulvinar condimentum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+					scelerisque facilisis orci at luctus. Donec in euismod nibh. Ut mollis enim neque, et
+					tincidunt nulla pulvinar condimentum.
+				</p>
+				<div class="btn">En savoir +</div>
+			</div>
+		</div>
+		<div class="slider">
+			<div class="swiper-container">
+				<div bind:this={wrapper} class="swiper-wrapper">
+					<div class="swiper-slide">
+						<img src="/images/expertise-yellow.png" alt="expertise-slide" />
+						<div class="btn">En savoir +</div>
+					</div>
+	
+					<div class="swiper-slide">
+						<img src="/images/expertise-yellow.png" alt="expertise-slide" />
+						<div class="btn">En savoir +</div>
+					</div>
+	
+					<div class="swiper-slide">
+						<img src="/images/expertise-yellow.png" alt="expertise-slide" />
+						<div class="btn">En savoir +</div>
+					</div>
+				</div>
+			</div>
+	
+			<div class="pagination" />
+		</div>
+	</div>
+	
 </div>
 
 <style lang="scss">
@@ -100,37 +252,36 @@
 			left: 6%;
 
 			h2 {
-					position: relative;
-					z-index: 1;
-					width: fit-content;
-				}
-			
+				position: relative;
+				z-index: 1;
+				width: fit-content;
+			}
 
-				h2:before {
-					background-color: rgba($color-bleu,.1);
-					display: block;
-					position: absolute;
-					left: -5%;
-					z-index: -1;
-    				bottom: 50%;
-					border-radius: 50px;
-					content: '';
-					width: 50px;
-					height: 50px;
-				}
+			h2:before {
+				background-color: rgba($color-bleu, 0.1);
+				display: block;
+				position: absolute;
+				left: -5%;
+				z-index: -1;
+				bottom: 50%;
+				border-radius: 50px;
+				content: '';
+				width: 50px;
+				height: 50px;
+			}
 
-				h2:after {
-					background-color: rgba($color-bleu,.1);
-					display: block;
-					position: absolute;
-					border-radius: 50px;
-					top: 25%;
-					left: 90%;
-					content: '';
-					z-index: -1;
-					width: 50px;
-					height: 50px;
-				}
+			h2:after {
+				background-color: rgba($color-bleu, 0.1);
+				display: block;
+				position: absolute;
+				border-radius: 50px;
+				top: 25%;
+				left: 90%;
+				content: '';
+				z-index: -1;
+				width: 50px;
+				height: 50px;
+			}
 			.bloc {
 				width: 50%;
 				padding: 32px;
@@ -172,4 +323,114 @@
 			}
 		}
 	}
+
+	.pink {
+		background-color: rgba($color-rose, 0.1);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 64px 0;
+		transition: all 0.3s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+
+		.links {
+			align-items: center;
+			width: 80%;
+			height: 64px;
+
+			@media only screen and (max-width: $phone) {
+				height: fit-content;
+			}
+
+			.link {
+				padding: 16px;
+				@media only screen and (max-width: $phone) {
+					border: none;
+				}
+
+				transition: all 0.3s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				border-left: $color-gris-dark solid 1px;
+				height: 100%;
+
+				&:nth-child(6) {
+					@media only screen and (min-width: $phone) {
+						border-right: $color-gris-dark solid 1px;
+					}
+				}
+			}
+		}
+
+		.contenu {
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			align-items: center;
+
+			.image {
+				flex-basis: 20%;
+				padding: 32px;
+				img {
+					width: 100%;
+					height: 100%;
+				}
+			}
+
+			.texte {
+				flex-basis: 60%;
+			}
+		}
+	}
+
+	.infoSlide{
+
+	display: flex;
+
+	.infos{
+		display: flex;
+		flex-basis: 60%;
+		justify-content: center;
+		align-items: start;
+		.medias{
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex-direction: column;
+			width: 30%;
+			img{
+				margin: 8px;
+				width: 64px;
+				height: 64px;
+			}
+		}
+
+		.texte{
+			width: 70%;
+			p{
+				margin-bottom: 64px;
+			}
+			.btn{
+				
+			}
+		}
+	}
+	.slider{
+		flex-basis: 40%;
+		max-width: 40%;
+
+		div
+		{
+			width: 100%;
+		}
+
+		img{
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+			background-size: cover;
+		}
+	}
+}
 </style>
