9ab3256ae2dcb78324ccf721e265d5c67c6c024f
Guillian Vibert <guillianvibert@MacBook-de-Guillian.local>
2023-11-26 23:28:09 +0100

    fix swiper

--- src/lib/components/SliderGomettes.svelte
+++ src/lib/components/SliderGomettes.svelte
@@ -1,211 +1,183 @@
-
-
 <script>
 	import { onMount } from 'svelte';
 	import Swiper from 'swiper';
-	import { Pagination,Navigation } from 'swiper/modules';
+	import { Pagination, Navigation } from 'swiper/modules';
 	import 'swiper/css';
 
-    export let collaborateurs;
+	export let collaborateurs;
 
+	let prenom;
+	let descriptionCourte;
+	let description;
 
-    let prenom;
-    let descriptionCourte;
-    let description;
-
-
-    collaborateurs?.sort((a,b)=> a.rang - b.rang).forEach((collaborateur, index) => {
-        collaborateur.partialId = index
-    })
-    
-	onMount((_) => {
-
-
-        if(collaborateurs == null || collaborateurs.length == 0)
-            return
-
-		let swiper = new Swiper('.swiper-container', {
-			modules: [Pagination,Navigation],
-            slidesPerGroup:1,
-            loop:true,
-            spaceBetween:0,
-            slidesPerView:1,
-            centeredSlides:true,
-            slideToClickedSlide:true,
-            breakpoints: {
-              
-                960: {
-                    slidesPerView: 3,
-                    spaceBetween: 30
-                },
-                // when window width is >= 640px
-                1150: {
-                    slidesPerView: 5,
-                    spaceBetween: 60
-                }
-            }
-		
+	collaborateurs
+		?.sort((a, b) => a.rang - b.rang)
+		.forEach((collaborateur, index) => {
+			collaborateur.partialId = index;
 		});
 
-      
-        function ChangeIndex(){
+	onMount((_) => {
+		if (collaborateurs == null || collaborateurs.length == 0) return;
 
+		let swiper = new Swiper('.swiper-container', {
+			modules: [Pagination, Navigation],
+			slidesPerGroup: 1,
+			loop: true,
+			spaceBetween: 0,
+			slidesPerView: 1,
+			centeredSlides: true,
+			slideToClickedSlide: true,
+			breakpoints: {
+				960: {
+					slidesPerView: 3,
+					spaceBetween: 30
+				},
+				// when window width is >= 640px
+				1150: {
+					slidesPerView: 5,
+					spaceBetween: 60,
 
-            swiper.slides.forEach((slid,index) => {
+					navigation: {
+						nextEl: '.swiper-button-next',
+						prevEl: '.swiper-button-prev'
+					}
+				}
+			}
+		});
 
-                
-                const id = slid.dataset.collaborateur
-                slid.firstElementChild.src =  collaborateurs[id].photoInactive
+		function ChangeIndex() {
+			swiper.slides.forEach((slid, index) => {
+				const id = slid.dataset.collaborateur;
+				slid.firstElementChild.src = collaborateurs[id].photoInactive;
+			});
 
-            })
-            
-            const slid = swiper.slides[swiper.activeIndex]
-            const id = slid.dataset.collaborateur
+			const slid = swiper.slides[swiper.activeIndex];
+			const id = slid.dataset.collaborateur;
 
-            prenom.innerHTML = collaborateurs[id].prenom;
-            descriptionCourte.innerHTML = collaborateurs[id].descriptionCourte;
-            description.innerHTML = collaborateurs[id].description;
-          
-            if(slid != null)
-                slid.firstElementChild.src =  collaborateurs[id].photoActive
+			prenom.innerHTML = collaborateurs[id].prenom;
+			descriptionCourte.innerHTML = collaborateurs[id].descriptionCourte;
+			description.innerHTML = collaborateurs[id].description;
 
-        }
+			if (slid != null) slid.firstElementChild.src = collaborateurs[id].photoActive;
+		}
 
-        swiper.on("activeIndexChange", ()=>{
+		swiper.on('activeIndexChange', () => {
+			ChangeIndex();
+		});
 
-            ChangeIndex()
-        })
-
-        ChangeIndex()
+		ChangeIndex();
 	});
-
-
 </script>
 
 {#if collaborateurs != null && collaborateurs.length != 0}
+	<div class="collaborateurs">
+		<div class="h2-seo center">
+			<h2 animate><span class="pink">Les Gommettes</span></h2>
+		</div>
 
-<div class="collaborateurs">
-
-    <div class="h2-seo center">
-		<h2 animate><span class="pink">Les Gommettes</span></h2>
-	</div>
-
-	<div class="swiper-collaborateur">
-		<div class="swiper-container">
-			<div class="swiper-wrapper">
-                {#each collaborateurs as collaborateur, index}
-                    <div class="swiper-slide" data-collaborateur="{collaborateur.partialId}">
-                        <img src="{collaborateur.photoInactive}" alt="{collaborateur.prenom}" />
-                    </div>
-                {/each}
-				
+		<div class="swiper-collaborateur">
+			<div class="swiper-container">
+				<div class="swiper-wrapper">
+					{#each collaborateurs as collaborateur, index}
+						<div class="swiper-slide" data-collaborateur={collaborateur.partialId}>
+							<img src={collaborateur.photoInactive} alt={collaborateur.prenom} />
+						</div>
+					{/each}
+				</div>
+				<div class="swiper-button swiper-button-next" />
+				<div class="swiper-button swiper-button-prev" />
 			</div>
-            <div class="swiper-button swiper-button-next"></div>
-            <div class="swiper-button swiper-button-prev"></div>
+		</div>
+
+		<div class="info">
+			<div class="collaborateur-info">
+				<h2 animate class="pink" bind:this={prenom} />
+				<h3 animate bind:this={descriptionCourte} />
+				<p animate bind:this={description} />
+			</div>
 		</div>
 	</div>
-
-    <div class="info">
-        <div class="collaborateur-info">
-
-            <h2 animate class="pink" bind:this={prenom}>
-            </h2>
-            <h3 animate bind:this={descriptionCourte}>
-            </h3>
-            <p animate bind:this={description}>
-            </p>
-    
-        </div>
-    </div>
- 
-</div>
-
 {/if}
 
-<style lang="scss" >
+<style lang="scss">
 	.collaborateurs {
-        overflow: hidden;
-        .info{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 0 32px 32px 32px;
+		overflow: hidden;
+		.info {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			padding: 0 32px 32px 32px;
 
-            @media only screen and (max-width:$phone) {
-                padding:0 8px 32px 8px;
-        		}
-            .collaborateur-info{
-                width: 60%;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                flex-direction: column;
+			@media only screen and (max-width: $phone) {
+				padding: 0 8px 32px 8px;
+			}
+			.collaborateur-info {
+				width: 60%;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				flex-direction: column;
 
-                h2{
-                    margin-bottom: 8px;
-                }
-                h3{
-                    font-family: $font-secondary-light;
-                    font-size: 1rem;
-                }
-                p{
-                    text-align: center;
-                }
-            }
-        }
+				h2 {
+					margin-bottom: 8px;
+				}
+				h3 {
+					font-family: $font-secondary-light;
+					font-size: 1rem;
+				}
+				p {
+					text-align: center;
+				}
+			}
+		}
 
+		.h2-seo {
+			padding: 64px 0 0 0;
+			margin-bottom: 0;
+			@media only screen and (max-width: $phone) {
+				padding: 32px;
+				margin: 0;
+			}
+			h2 {
+				@media only screen and (max-width: $phone) {
+					font-size: 2.5rem;
+				}
 
-        .h2-seo{
-            padding: 64px 0 0 0;
-            margin-bottom: 0;
-            @media only screen and (max-width:$phone) {
-                padding: 32px;
-                margin: 0;
-        		}
-            h2{
-
-                @media only screen and (max-width:$phone) {
-                    font-size: 2.5rem;
-        		}
-
-                font-size: 4rem;
-            }
-        }
+				font-size: 4rem;
+			}
+		}
 
 		.swiper-collaborateur {
-            width: 100%;
+			width: 100%;
 			display: flex;
 			justify-content: center;
 			align-items: center;
 			.swiper-container {
 				width: 100%;
-                height: 40vh; 
-                overflow: visible;
-
+				height: 40vh;
+				overflow: visible;
 
 				.swiper-wrapper {
 					.swiper-slide {
-                        cursor: pointer;
-                        transition: all .3s cubic-bezier(0.55, 0.055, 0.675, 0.19);
-                        padding: 64px;
+						cursor: pointer;
+						transition: all 0.3s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+						padding: 64px;
 
-                        @media only screen and (max-width:$phone) {
-                        font-size: 2.5rem;
-                    }
-
+						@media only screen and (max-width: $phone) {
+							font-size: 2.5rem;
+						}
 
 						img {
-                      
 							width: 100%;
 							height: 100%;
 							object-fit: contain;
 
-                            // -webkit-mask-image: url("/images/bubble-mask.svg");
-                            // mask-image: url("/images/bubble-mask.svg");
-                            // -webkit-mask-size: contain;
-                            // mask-size: contain;
-                            // -webkit-mask-repeat: no-repeat;
-                            // mask-repeat: no-repeat;
+							// -webkit-mask-image: url("/images/bubble-mask.svg");
+							// mask-image: url("/images/bubble-mask.svg");
+							// -webkit-mask-size: contain;
+							// mask-size: contain;
+							// -webkit-mask-repeat: no-repeat;
+							// mask-repeat: no-repeat;
 						}
 					}
 				}
