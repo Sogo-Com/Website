f1933a4154b2e53d0eb82107060b2a20d926d4ff
GuillianV <guillianvibert90@gmail.com>
2024-01-14 12:45:24 +0100

    feat: donnees + mentions

--- src/routes/(front)/donnees-personelles/+page.svelte
+++ src/routes/(front)/donnees-personelles/+page.svelte
@@ -1,9 +1,147 @@
 <script>
+	
+	export let data;
+	const { donneesPerso = {} } = data;
 
-	import { onMount } from 'svelte';
 
-	onMount((_) => {
-
-	});
 </script>
 
+<div class="donnees">
+	<div>
+		<div class="privacy-policy">
+			<div>
+				<h1>POLITIQUE DE CONFIDENTIALITÉ</h1>
+				<p>
+					<br /><br /><strong>{donneesPerso["$website$"]}</strong><br />{donneesPerso["$nomEntreprise$"]}<br /><br />
+					Type de site : vitrine
+				</p>
+			</div>
+
+			<div>
+				<h2>Le but de cette politique de confidentialité</h2>
+				<p>
+					Le but de cette politique de confidentialité est d'informer les utilisateurs de notre site
+					des données personnelles que nous recueillerons ainsi que les informations suivantes, le
+					cas échéant :
+				</p>
+				<ol start="1" class="list-style-type-lower-alpha">
+					<li><span>Les données personnelles que nous recueillerons</span></li>
+					<li><span>L’utilisation des données recueillies</span></li>
+					<li><span>Qui a accès aux données recueillies</span></li>
+					<li><span>Les droits des utilisateurs du site</span></li>
+					<li><span>La politique de cookies du site</span></li>
+				</ol>
+				<p>
+					Cette politique de confidentialité fonctionne parallèlement aux conditions générales
+					d’utilisation de notre site.
+				</p>
+			</div>
+
+			<!-- Ajoutez d'autres sections de votre politique de confidentialité ici -->
+
+			<div>
+				<h2>Consentement</h2>
+				<p>Les utilisateurs conviennent qu’en utilisant notre site, ils consentent à :</p>
+				<ol start="1" class="list-style-type-lower-alpha">
+					<li>
+						<span>les conditions énoncées dans la présente politique de confidentialité et</span>
+					</li>
+					<li>
+						<span
+							>la collecte, l’utilisation et la conservation des données énumérées dans la présente
+							politique.</span
+						>
+					</li>
+				</ol>
+			</div>
+
+			<div>
+				<!-- Ajoutez d'autres sections de votre politique de confidentialité ici -->
+			</div>
+
+			<div>
+				<h2>Politique sur les cookies</h2>
+				<p>
+					Un cookie est un petit fichier, stocké sur le disque dur d’un utilisateur par le site Web.
+					Son but est de recueillir des données relatives aux habitudes de navigation de
+					l’utilisateur.
+				</p>
+				<p>Nous utilisons les types de cookies suivants sur notre site :</p>
+				<ol start="1" class="list-style-type-lower-alpha">
+					<li>
+						<strong><span>Cookies analytiques</span></strong><br />
+						Cela nous permet d’améliorer la conception et la fonctionnalité de notre site en recueillant
+						des données sur le contenu auquel vous accédez et sur lequel vous vous accrochez en utilisant
+						notre site.
+					</li>
+				</ol>
+				<p>
+					Vous pouvez choisir d’être averti chaque fois qu’un cookie est transmis. Vous pouvez
+					également choisir de désactiver les cookies entièrement dans votre navigateur Internet,
+					mais cela peut diminuer la qualité de votre expérience d’utilisation.
+				</p>
+			</div>
+
+			<div>
+				<!-- Ajoutez d'autres sections de votre politique de confidentialité ici -->
+			</div>
+
+			<div>
+				<h2>Modifications</h2>
+				<p>
+					Cette politique de confidentialité peut être modifiée à l’occasion afin de maintenir la
+					conformité avec la loi et de tenir compte de tout changement à notre processus de collecte
+					de données. Nous recommandons à nos utilisateurs de vérifier notre politique de temps à
+					autre pour s’assurer qu’ils soient informés de toute mise à jour. Au besoin, nous pouvons
+					informer les utilisateurs par courriel des changements apportés à cette politique.
+				</p>
+			</div>
+
+			<div>
+				<h2>Contact</h2>
+				<p>
+					Si vous avez des questions à nous poser, n’hésitez pas à communiquer avec nous en
+					utilisant ce qui suit :<br />
+					{donneesPerso["$representantCnil$"]}<br />
+					{donneesPerso["$adresseEntreprise$"]}
+				</p>
+			</div>
+
+			<p>
+				<br /><em>Date d’entrée en vigueur : </em>le 14 janvier 2024
+			</p>
+		</div>
+	</div>
+</div>
+
+
+<style lang="scss">
+
+	.donnees{
+		margin: 0 auto;
+		max-width: 800px;
+		padding: 150px 20px;
+
+		h1,h2{
+			color: $color-gris-dark;
+			margin: 32px;
+		}
+
+		a{
+			color: $color-bordeaux;
+			text-underline-offset: 5px;
+		}
+
+		strong{
+			font-weight: 600;
+		}
+		ol{
+			margin: 32px;
+			li{
+				margin: 16px 0;
+				font-family: $font-secondary-light;
+			}
+		}
+	}
+
+</style>
---
+++ src/routes/(front)/mentions/+page.server.js
@@ -0,0 +1,38 @@
+
+
+const mentionsLegales = {
+	$website$: 'https://www.sogocom.fr',
+	$formJuridique$: 'SARL',
+	$nomEntreprise$: 'SOGOCOM',
+	$csEntreprise$: '4000,00',
+	$tvaEntreprise$: 'FR57511682387',
+	$adresseEntreprise$: '356 RTE DE CHAVANNE 74330 POISY',
+	$cpEntreprise$: '74330',
+	$villeEntreprise$: 'POISY',
+    $nomWebmaster$ : "Vibert-vallet Guillian",
+    $mailWebmaster$ : "vibert.vallet.guillian@gmail.com",
+    $nomHebergeur$ : "OVH",
+    $adresseHebergeur$ : "2 rue Kellermann 59100 Roubaix",
+    $telHebergeur$ : "1007",
+    $cpHebergeur$ : "59100",
+    $villeHebergeur$ : "Roubaix",
+    $representantCnil$ : "Vibert-vallet Guillian",
+    $mailCnil$ : "vibert.vallet.guillian@gmail.com",
+    $representantEntreprise$ : "Sophie GOMME - Arnaud TROGNEE",
+    $donneesCnil$ : "Nom, prénom, adresse, téléphone, email",
+    $autreDonneesCnil$ : "Cookies",
+    $conservationCnil$ : "5 ans",
+    $securiteRGPD$ : `Afin d’assurer la protection de votre sécurité, nous utilisons le protocole de sécurité de la couche transport pour transmettre des renseignements personnels dans notre système.
+    Toutes les données stockées dans notre système sont bien sécurisées et ne sont accessibles qu’à nos employés. Nos employés sont liés par des accords de confidentialité stricts et une violation de cet accord entraînerait le licenciement de l'employé.`
+    
+};
+
+
+export const load = async (serverloadEvent) => {
+	return {
+		mentionsLegales
+	};
+};
+
+
+
